6a02ab87cf3e30d2f440d1d8dc53383b2ceadd9f
hnhillol <37249884+hnhillol@users.noreply.github.com>
2018-12-24 04:15:19 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 24 02:11:25 2018
+
+@author: hnhillol , Habib Un Nabi Hillol
+Homework Assignment #1
+To represent different types of variables ,This program is printing maximum possible meta Data of 
+a song and its different attributes. 
+"""
+SongName= "Here comes the Sun" # First line of the song
+BandName="The Beatles" # The famous Band of the '70s
+Artist ="George Harrison, John Lennon , Paul McCartney" # Its band song ang by Multiple singers , not a Solo Song
+Album = "Abbey Road"
+Publisher="Harrisongs" # Publsihers name or the recording studio name
+ReleasedDate=26 
+ReleaseMonths="September"
+ReleaseYear=1969
+RecordedPeriod="7th July-19th August"
+Genre="Folk pop , Pop Rock" # It can be Genred in two type
+Length=3.06 # in Minute and Second
+Lable= "Apple"
+Writter="George Harrison" # He is also famous for his solo Album Concert for Bangladesh
+Producer="George Martin"
+"""
+Below the output of all the above attributes of the song will be printed
+Though the listis ver Big 
+but this is possible to compose in the sake of learning 
+"""
+
+print(SongName)
+
+print(BandName)
+
+print(Artist)
+
+print(Album)
+
+print(Publisher)
+
+print(ReleasedDate)
+
+print(ReleaseYear)
+
+print(RecordedPeriod)
+
+print(Genre)
+
+print(Length)
+
+print(Lable)
+
+print(Writter)
+
+print(Producer)
+
+
+
